fe85e0e9723d8e3756fdc99dca888a46a12f6cad
Taif Rahim <77005246+nefario19@users.noreply.github.com>
2021-03-04 09:07:41 +0100

    Version 2

---
+++ Assignment/src/main/kotlin/main.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+    //1. Create a class and name it Player
+
+    /*2. Modify the Player class so that it's constructor allows different values for the
+         name, lives, level and score properties.
+     */
+
+    /*3.Modify the Player class so that the properties have default values:
+        lives = 3
+        level = 1
+        score = 0
+     */
+
+    /*4. Create two new instances of the Player class. You can call them anything you like:
+
+        The first instance should have a level of 4, and 8 lives
+
+        The second one should have a level of 2, 5 lives and a score of 1000
+     */
+
+    //5. Use the show function to print out the values for your new instances
+
+    /*Note: When you add parameters that have default values, they must be listed after any parameters that don't.
+            So name must be the first parameter followed by the other parameters.
+     */
+}
